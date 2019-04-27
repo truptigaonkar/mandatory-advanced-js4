4976d17636661ccd6aeb259ecf9c3cd2b3428985
truptigaonkar <trups11.trupti@gmail.com>
2019-04-27 10:08:08 +0200

    Updates

---
+++ precache-manifest.4363faaca3c6303404fa5e59c6b30a72.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mandatory-advanced-js4/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "0c02fdccd987cb5f944a",
+    "url": "/mandatory-advanced-js4/static/css/main.f6d4fb26.chunk.css"
+  },
+  {
+    "revision": "eb147c3d7ae77fe3eefb",
+    "url": "/mandatory-advanced-js4/static/js/runtime~main.78de8069.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mandatory-advanced-js4/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "009847db7dde7b05ecc2",
+    "url": "/mandatory-advanced-js4/static/js/2.ec6dc4c3.chunk.js"
+  },
+  {
+    "revision": "64b558c156f9808a3734dfd2e94d544f",
+    "url": "/mandatory-advanced-js4/static/media/image1.64b558c1.jpeg"
+  },
+  {
+    "revision": "0c02fdccd987cb5f944a",
+    "url": "/mandatory-advanced-js4/static/js/main.2cda0891.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mandatory-advanced-js4/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mandatory-advanced-js4/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mandatory-advanced-js4/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "009847db7dde7b05ecc2",
+    "url": "/mandatory-advanced-js4/static/css/2.3efbd340.chunk.css"
+  },
+  {
+    "revision": "059dc56a78c6dcf9525f38a7bf6214ff",
+    "url": "/mandatory-advanced-js4/index.html"
+  }
+];
